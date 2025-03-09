f03561d3a1d2a4fc0f7d385b3692716c4ded3584
trancedj2022 <134460924+trancedj2022@users.noreply.github.com>
2025-03-09 13:29:24 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+export UUID=${UUID:-'c197463f-6cc7-4b48-ba7f-088426b9a30f'} 
+export NEZHA_SERVER=${NEZHA_SERVER:-''} # 哪吒3个变量不全不运行
+export NEZHA_PORT=${NEZHA_PORT:-'443'} # 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls    
+export NEZHA_KEY=${NEZHA_KEY:-''}      # 哪吒客户端密钥
+export ARGO_DOMAIN=${ARGO_DOMAIN:-''}  # 固定隧道域名，留空即启用临时隧道
+export ARGO_AUTH=${ARGO_AUTH:-''}     # 固定json或token，留空即启用临时隧道
+export CFIP=${CFIP:-'na.ma'}          # 优选域名或优选ip
+export CFPORT=${CFPORT:-'443'}       # 节点端口
+export NAME=${NAME:-''}            # 节点名称前缀
+export FILE_PATH=${FILE_PATH:-'./temp'} # 节点文件存放路径，运行文件夹，若需要改，index.js中也需要和此处一致，否则无法订阅
+export ARGO_PORT=${ARGO_PORT:-'8001'}  # Argo端口，使用固定隧道token需在cf后台设置的端口和这里一致
+
+if [ ! -d "${FILE_PATH}" ]; then
+    mkdir ${FILE_PATH}
+fi
+
+cleanup_oldfiles() {
+  rm -rf ${FILE_PATH}/boot.log ${FILE_PATH}/sub.txt ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
+}
+cleanup_oldfiles
+wait
+
+argo_configure() {
+  if [[ -z $ARGO_AUTH || -z $ARGO_DOMAIN ]]; then
+    echo -e "\e[1;32mARGO_DOMAIN or ARGO_AUTH variable is empty, use quick tunnels\e[0m"
+    return
+  fi
+
+  if [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+    echo $ARGO_AUTH > ${FILE_PATH}/tunnel.json
+    cat > ${FILE_PATH}/tunnel.yml << EOF
+tunnel: $(cut -d\" -f12 <<< "$ARGO_AUTH")
+credentials-file: ${FILE_PATH}/tunnel.json
+protocol: http2
+
+ingress:
+  - hostname: $ARGO_DOMAIN
+    service: http://localhost:$ARGO_PORT
+    originRequest:
+      noTLSVerify: true
+  - service: http_status:404
+EOF
+  else
+    echo -e "\e[1;32mARGO_AUTH mismatch TunnelSecret,use token connect to tunnel\e[0m"
+  fi
+}
+argo_configure
+wait
+
+generate_config() {
+  cat > ${FILE_PATH}/config.json << EOF
+{
+  "log": { "access": "/dev/null", "error": "/dev/null", "loglevel": "none" },
+  "inbounds": [
+    {
+      "port": $ARGO_PORT,
+      "protocol": "vless",
+      "settings": {
+        "clients": [{ "id": "${UUID}", "flow": "xtls-rprx-vision" }],
+        "decryption": "none",
+        "fallbacks": [
+          { "dest": 3001 }, { "path": "/vless-argo", "dest": 3002 },
+          { "path": "/vmess-argo", "dest": 3003 }, { "path": "/trojan-argo", "dest": 3004 }
+        ]
+      },
+      "streamSettings": { "network": "tcp" }
+    },
+    {
+      "port": 3001, "listen": "127.0.0.1", "protocol": "vless",
+      "settings": { "clients": [{ "id": "${UUID}" }], "decryption": "none" },
+      "streamSettings": { "network": "ws", "security": "none" }
+    },
+    {
+      "port": 3002, "listen": "127.0.0.1", "protocol": "vless",
+      "settings": { "clients": [{ "id": "${UUID}", "level": 0 }], "decryption": "none" },
+      "streamSettings": { "network": "ws", "security": "none", "wsSettings": { "path": "/vless-argo" } },
+      "sniffing": { "enabled": true, "destOverride": ["http", "tls", "quic"], "metadataOnly": false }
+    },
+    {
+      "port": 3003, "listen": "127.0.0.1", "protocol": "vmess",
+      "settings": { "clients": [{ "id": "${UUID}", "alterId": 0 }] },
+      "streamSettings": { "network": "ws", "wsSettings": { "path": "/vmess-argo" } },
+      "sniffing": { "enabled": true, "destOverride": ["http", "tls", "quic"], "metadataOnly": false }
+    },
+    {
+      "port": 3004, "listen": "127.0.0.1", "protocol": "trojan",
+      "settings": { "clients": [{ "password": "${UUID}" }] },
+      "streamSettings": { "network": "ws", "security": "none", "wsSettings": { "path": "/trojan-argo" } },
+      "sniffing": { "enabled": true, "destOverride": ["http", "tls", "quic"], "metadataOnly": false }
+    }
+  ],
+  "dns": { "servers": ["https+local://8.8.8.8/dns-query"] },
+   "outbounds": [
+        {
+            "protocol": "freedom",
+            "tag": "direct"
+        },
+        {
+            "protocol": "blackhole",
+            "tag": "block"
+        }
+    ]
+}
+EOF
+}
+generate_config
+wait
+
+ARCH=$(uname -m) && DOWNLOAD_DIR="${FILE_PATH}" && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
+if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
+    FILE_INFO=("https://github.com/eooce/test/releases/download/ARM/server bot" "https://github.com/eooce/test/releases/download/ARM/web web" "https://github.com/trancedj2022/test/releases/download/arm64/swith npm")
+elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
+    FILE_INFO=("https://github.com/eooce/test/raw/main/server bot" "https://github.com/eooce/test/raw/main/web web" "https://github.com/trancedj2022/test/releases/download/amd64/swith npm")
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+for entry in "${FILE_INFO[@]}"; do
+    URL=$(echo "$entry" | cut -d ' ' -f 1)
+    NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
+    FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
+    if [ -e "$FILENAME" ]; then
+        echo -e "\e[1;32m$FILENAME already exists,Skipping download\e[0m"
+    else
+        curl -L -sS -o "$FILENAME" "$URL"
+        echo -e "\e[1;32mDownloading $FILENAME\e[0m"
+    fi
+done
+wait
+
+run() {
+  if [ -e "${FILE_PATH}/npm" ]; then
+    chmod 777 "${FILE_PATH}/npm"
+    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
+    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
+      NEZHA_TLS="--tls"
+    else
+      NEZHA_TLS=""
+    fi
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+        nohup "${FILE_PATH}/npm" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+		   sleep 2
+        echo -e "\e[1;32mnpm is running\e[0m"
+    else
+        echo -e "\e[1;35mNEZHA variable is empty,skiping runing\e[0m"
+    fi
+  fi
+
+  if [ -e "${FILE_PATH}/web" ]; then
+    chmod 777 "${FILE_PATH}/web"
+    nohup "${FILE_PATH}/web" -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
+	 sleep 2
+    echo -e "\e[1;32mweb is running\e[0m"
+  fi
+
+  if [ -e "${FILE_PATH}/bot" ]; then
+    chmod 777 "${FILE_PATH}/bot"
+    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
+    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+      args="tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run"
+    else
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:$ARGO_PORT"
+    fi
+    nohup "${FILE_PATH}/bot" $args >/dev/null 2>&1 &
+    sleep 2
+    echo -e "\e[1;32mbot is running\e[0m"
+  fi
+} 
+run
+sleep 5
+
+function get_argodomain() {
+  if [[ -n $ARGO_AUTH ]]; then
+    echo "$ARGO_DOMAIN"
+  else
+    grep -oE 'https://[[:alnum:]+\.-]+\.trycloudflare\.com' "${FILE_PATH}/boot.log" | sed 's@https://@@'
+  fi
+}
+
+generate_links() {
+  argodomain=$(get_argodomain)
+  echo -e "\e[1;32mArgoDomain:\e[1;35m${argodomain}\e[0m"
+  sleep 2
+
+  isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
+  sleep 2
+
+  VMESS="{ \"v\": \"2\", \"ps\": \"${NAME}-${isp}\", \"add\": \"${CFIP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${argodomain}\", \"path\": \"/vmess-argo?ed=2048\", \"tls\": \"tls\", \"sni\": \"${argodomain}\", \"alpn\": \"\" }"
+
+  cat > ${FILE_PATH}/list.txt <<EOF
+vless://${UUID}@${CFIP}:${CFPORT}?encryption=none&security=tls&sni=${argodomain}&type=ws&host=${argodomain}&path=%2Fvless-argo%3Fed%3D2048#${NAME}-${isp}
+
+vmess://$(echo "$VMESS" | base64 -w0)
+
+trojan://${UUID}@${CFIP}:${CFPORT}?security=tls&sni=${argodomain}&type=ws&host=${argodomain}&path=%2Ftrojan-argo%3Fed%3D2048#${NAME}-${isp}
+EOF
+
+  base64 -w0 ${FILE_PATH}/list.txt > ${FILE_PATH}/sub.txt
+  cat ${FILE_PATH}/sub.txt
+  echo -e "\n\e[1;32m${FILE_PATH}/sub.txt saved successfully\e[0m"
+  sleep 5  
+  rm -rf ${FILE_PATH}/list.txt ${FILE_PATH}/boot.log ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
+}
+generate_links
+echo -e "\e[1;96mRunning done!\e[0m"
+echo -e "\e[1;96mThank you for using this script,enjoy!\e[0m"
+sleep 10
+clear
+
+echo -e "\e[1;96mApp is running!\e[0m"
